0647fa02ceca6d11acd99869b8f484f23f6b7857
GoldenLin9 <goldenlin0909@gmail.com>
2024-05-25 23:38:29 -0400

    positions header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
+import "./styles/app.css"
 
 function App() {
     return (
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/main.css"
+
+export default function Main() {
+  return (
+    <main>
+
+        <div id = "hero">
+
+          <h1>PureLight Real Estate Wholesaling</h1>
+        </div>
+
+    </main>
+  )
+}
